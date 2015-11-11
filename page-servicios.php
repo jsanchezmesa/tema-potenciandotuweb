96a34ce4feea709f9224ebab3d60b4efd962191b
Juan Sánchez Mesa <sanchezmesaj@gmail.com>
2015-11-11 19:15:21 +0100

    Actualización de páginas de servicios y contacto

--- page-servicios.php
+++ page-servicios.php
@@ -23,7 +23,7 @@ get_header();
 			<h3>
 				<ul>
 					<li>Creación del diseño de tu web desde cero y completamente personalizado.</li>
-					<li>Modifica´ción de tu web ya creada para adaptarla a los patrones de diseño actuales.</li>
+					<li>Modificación de tu web ya creada para adaptarla a los patrones de diseño actuales.</li>
 					<li>Todos los diseños son "responsive" para que se vean de forma adecuada independientemente del dispositivo donde se visualice.</li>
 				</ul>
 			</h3>
@@ -40,7 +40,8 @@ get_header();
 				<ul>
 					<li>Utilización de las más modernas tecnologías existentes.</li>
 					<li>Haz que tu página se vea en todo tipo de dispositivo sustituyendo Flash por HTML5.</li>
-					<li>Usa esquinas redondeadas, sombras, menús desplegables, etc. gracias a CSS3.</li>
+					<li>Usa esquinas redondeadas, sombras, etc. gracias a CSS3.</li>
+					<li>Uso de Javascript y jQuery para conseguir sorprendentes efectos como menús desplegables.</li>
 				</ul>
 			</h3>
 		</div>
@@ -51,12 +52,13 @@ get_header();
 			<img src="<?php bloginfo('stylesheet_directory'); ?>/images/wordpress.png" alt="WordPress" title="Las últimas tecnologías" width="800" height="600" >
 		</div>
 		<div class="home-text">
-			<h2>Desarrollo web</h2>
+			<h2>Uso de WordPress</h2>
 			<h3>
 				<ul>
 					<li>Uso de la plataforma WordPress para construir tu web ahorrándote mucho dinero. Esta tecnología no se limita a blogs.</li>
 					<li>Posibilidad de elegir un tema ya existente o si lo prefieres te lo construimos a medida para ti.</li>
 					<li>La plataforma WordPress te permite administrar el contenido de tu web por ti mismo.</li>
+					<li>WordPress es usado por millones de webs en todo el mundo, por lo que es una plataforma probada en rendimiento y seguridad.</li>
 				</ul>
 			</h3>
 		</div>
